348b18b4b217e1693adf3c4728ad41fa8bc1a7d7
Dimitris Panokostas <dimitris.panokostas@liseberg.se>
2016-02-11 16:49:38 +0100

    Initial commit, still pretty much an empty project

---
+++ MB_SubSonic/SubSonicPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBeePlugin
+{
+    public partial class Plugin
+    {
+
+    }
+}
