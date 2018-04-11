51c0f61d4affbb270b7c23749122c6b4bf818708
Dimitris Panokostas <midwan@gmail.com>
2018-04-11 21:38:54 +0200

    Bumped assembly version to v2.14 also

--- MB_SubSonic/Properties/AssemblyInfo.cs
+++ MB_SubSonic/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Blitter Studio")]
 [assembly: AssemblyProduct("MB_SubSonic")]
-[assembly: AssemblyCopyright("Copyright © Dimitris Panokostas 2016-2017")]
+[assembly: AssemblyCopyright("Copyright © Dimitris Panokostas 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.11.0.0")]
-[assembly: AssemblyFileVersion("2.11.0.0")]
+[assembly: AssemblyVersion("2.14.0.0")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
