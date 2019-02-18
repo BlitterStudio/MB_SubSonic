75851b8200c46a0c84e13026298b5ad7ffdced1c
Dimitris Panokostas <midwan@gmail.com>
2019-02-18 20:34:41 +0100

    Bumped version to v2.25

--- MB_SubSonic/Plugin.cs
+++ MB_SubSonic/Plugin.cs
@@ -34,7 +34,7 @@ namespace MusicBeePlugin
             // current only applies to artwork, lyrics or instant messenger name that appears in the provider drop down selector or target Instant Messenger
             _about.Type = Interfaces.Plugin.PluginType.Storage;
             _about.VersionMajor = 2; // your plugin version
-            _about.VersionMinor = 20;
+            _about.VersionMinor = 25;
             _about.Revision = 0;
             _about.MinInterfaceVersion = Interfaces.Plugin.MinInterfaceVersion;
             _about.MinApiRevision = Interfaces.Plugin.MinApiRevision;
--- MB_SubSonic/Properties/AssemblyInfo.cs
+++ MB_SubSonic/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Blitter Studio")]
 [assembly: AssemblyProduct("MB_SubSonic")]
-[assembly: AssemblyCopyright("Copyright © Dimitris Panokostas 2016-2018")]
+[assembly: AssemblyCopyright("Copyright © Dimitris Panokostas 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.20.0.0")]
-[assembly: AssemblyFileVersion("2.20.0.0")]
+[assembly: AssemblyVersion("2.25.0.0")]
+[assembly: AssemblyFileVersion("2.25.0.0")]
